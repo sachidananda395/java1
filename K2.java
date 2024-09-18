6654ccca5701ead5005c2249aa4a0612dd739979
sachidananda395 <sachidananda.23cs124@sode-edu.in>
2024-09-18 21:42:46 +0530

    Add files via upload

---
+++ K2.java
@@ -0,0 +1,16 @@
+public class K2{
+public static void main(String[] args){
+int n1=8;
+int n2=4;
+int sum=n1+n2;
+int diff=n1-n2;
+int prd=n1*n2;
+int div=n1/n2;
+int rem=n1%n2;
+System.out.println(sum);
+System.out.println(diff);
+System.out.println(prd);
+System.out.println(div);
+System.out.println(rem);
+}
+}

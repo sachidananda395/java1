5ec20913566730a14c1d99f75a45a64f1bbb8358
sachidananda395 <sachidananda.23cs124@sode-edu.in>
2024-09-24 19:40:12 +0530

    Add files via upload

---
+++ MultiplicationTables.java
@@ -0,0 +1,10 @@
+public class MultiplicationTables {
+    public static void main(String[] args) {
+       for (int i = 3; i <= 20; i++) {
+          for (int j = 1; j <= 10; j++) {
+            System.out.println(i + " * " + j + " = " + (i * j));
+            }
+            System.out.println();
+        }
+    }
+}
